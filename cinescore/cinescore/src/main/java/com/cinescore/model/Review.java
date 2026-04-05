8bdc89ddcf7e0af58e8f8b693335444c76b3cfd9
Victor Daigo <victordaigo0603@gmail.com>
2026-04-05 19:25:08 -0300

    corrige LazyInitializationException no relacionamento User em Review

--- cinescore/cinescore/src/main/java/com/cinescore/model/Review.java
+++ cinescore/cinescore/src/main/java/com/cinescore/model/Review.java
@@ -28,7 +28,7 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
